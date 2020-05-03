32cafbce6f405d2bbcb4b30fb0cddbad2a4ee814
Karl Lorey <git@karllorey.com>
2020-05-03 22:22:34 +0200

    First views

--- socials_api_api/urls.py
+++ socials_api_api/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 app_name = "socials_api_api"
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    path("", views.api_root),
+    path("filter-url", views.filter_url, name="filter-url"),
+    path("filter-url-list", views.filter_url_list, name="filter-url-list"),
+    path("fetch-url", views.fetch_url, name="fetch-url"),
+]
--- socials_api_api/views.py
+++ socials_api_api/views.py
@@ -1,8 +1,35 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+import socials
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
-def index(request):
-    return JsonResponse({"msg": "This is the socials API."})
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "fetch-url": reverse("socials_api_api:fetch-url", request=request, format=format),
+            "filter-url-list": reverse(
+                "socials_api_api:filter-url-list", request=request, format=format
+            ),
+            "filter-url": reverse("socials_api_api:filter-url", request=request, format=format),
+        }
+    )
+
+
+@api_view(["POST"])
+def fetch_url(request):
+    raise NotImplementedError()
+
+
+@api_view(["POST"])
+def filter_url_list(request):
+    raise NotImplementedError()
+
+
+@api_view(["POST"])
+def filter_url(request):
+    url = request.POST.get("url")
+    return Response(socials.extract([url]).get_matches_per_platform())
